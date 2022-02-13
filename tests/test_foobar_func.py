a2035e8487eb1eb0285a56417ff8aaff7fc38991
Andrey Smekhov <myfearismyblue@gmail.com>
2022-02-13 23:05:16 +0300

    Added exceptions test Added .pytest_cache/ to .gitignore

--- tests/test_foobar_func.py
+++ tests/test_foobar_func.py
@@ -1,8 +1,17 @@
 from utils import foobar
 import pytest
 
-@pytest.mark.parametrize("dividend, deviser, expected_value", [(1, 1, 1.0),
+
+@pytest.mark.parametrize("dividend, devisor, expected_value", [(1, 1, 1.0),
                                                                (0.5, 0.5, 1.0),
                                                                ])
-def test_foobar_positives(dividend, deviser, expected_value):
-    assert foobar(dividend, deviser) == expected_value
+def test_foobar_positives(dividend, devisor, expected_value):
+    assert foobar(dividend, devisor) == expected_value
+
+
+@pytest.mark.parametrize("dividend, devisor, expected_exception", [(0, 0, ZeroDivisionError),
+                                                                   ("1", 1, TypeError),
+                                                                   (1, "1", TypeError)])
+def test_foobar_exceptions(dividend, devisor, expected_exception):
+    with pytest.raises(expected_exception):
+        foobar(dividend, devisor)
